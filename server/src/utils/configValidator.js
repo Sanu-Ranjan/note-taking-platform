c913f2bb0cbbf1c15c4e12fc374e99b4af3fd66d
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-09-01 23:29:03 +0530

    feat: add config validator for databse

---
+++ server/src/utils/configValidator.js
@@ -0,0 +1,25 @@
+const Joi = require("joi");
+const config = require("config");
+
+const schema = Joi.object({
+  mysqlDB: Joi.object({
+    host: Joi.string().required(),
+    port: Joi.number().required(),
+    database: Joi.string().required(),
+    user: Joi.string().required(),
+    password: Joi.string().required(),
+    ssl: Joi.any().optional(),
+  }).required(),
+  server: Joi.object({
+    port: Joi.number().required(),
+  }).required(),
+});
+
+const { error } = schema.validate(config, { allowUnknown: true });
+
+if (error) {
+  throw new Error(
+    `Refer .env.example, Config validation error: ${error.message}`
+  );
+}
+console.log("Config validated successfully");
