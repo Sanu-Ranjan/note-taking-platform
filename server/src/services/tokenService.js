719564afc20de8d3540a44e51882790c77194621
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-09-05 22:42:11 +0530

    feat: add token service (add sign jwt)

---
+++ server/src/services/tokenService.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+let secret = process.env.JWT_SECRET;
+let expiry = process.env.JWT_ASCESS_EXPIRES;
+
+if (!secret) {
+  console.log("fatal error: jwt secret not defined");
+  process.exit(1);
+}
+
+function signToken(payload) {
+  const token = jwt.sign(payload, config.get(secret), {
+    expiresIn: expiry || "5d",
+  });
+}
+
+module.exports = {
+  signToken,
+};
