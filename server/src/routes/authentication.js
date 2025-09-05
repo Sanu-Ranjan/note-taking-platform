d4b7af3975d876a8f7e69ad5907c9f51491ea424
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-09-05 15:14:27 +0530

    refactor : change route handler name

--- server/src/routes/authentication.js
+++ server/src/routes/authentication.js
@@ -1,8 +1,8 @@
-const { redirect, getUser } = require("../controllers/authentication");
+const { consentRedirect, getUser } = require("../controllers/authentication");
 
 const router = require("express").Router();
 
-router.get("/google", redirect);
+router.get("/google", consentRedirect);
 router.get("/google/callback", getUser);
 //router.post("/refresh",);
 //router.post("/logout",);
