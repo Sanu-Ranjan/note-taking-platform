37d777bbe009da80d60ca452b9546397c50c76ea
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-09-05 13:49:54 +0530

    feat(update): using state parameter

--- server/src/controllers/authentication.js
+++ server/src/controllers/authentication.js
@@ -1,8 +1,21 @@
-const { url, oauth2Client } = require("../services/googleAuthService");
+const {
+  generateAuthUrl,
+  oauth2Client,
+} = require("../services/googleAuthService");
 const { googleClient } = require("../utils");
+const crypto = require("crypto");
+
+const isProd = process.env.NODE_ENV === "production";
 
 const redirect = (req, res) => {
-  res.redirect(url);
+  const state = crypto.randomBytes(16).toString("hex");
+  res.cookie("oauth_state", state, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: isProd,
+    maxAge: 5 * 60 * 1000,
+  });
+  res.redirect(generateAuthUrl(state));
 };
 
 const getUser = async (req, res) => {
@@ -11,7 +24,7 @@ const getUser = async (req, res) => {
     console.log(error);
     const { tokens } = await oauth2Client.getToken(code);
 
-    const ticket = await oauth2Client.verifyIdToken({
+    const ticket = oauth2Client.verifyIdToken({
       idToken: tokens.id_token,
       audience: googleClient.client_id,
     });
