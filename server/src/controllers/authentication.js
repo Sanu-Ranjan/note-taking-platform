c2c187c2e795c37376bf40410c6cefbb125a12d7
Sanu-Ranjan <ranjan.code33@gmail.com>
2025-09-09 21:56:25 +0530

    feat: rorate refresh token implemented

--- server/src/controllers/authentication.js
+++ server/src/controllers/authentication.js
@@ -1,3 +1,4 @@
+const { where, Sequelize } = require("sequelize");
 const { RefreshTokens } = require("../models");
 const {
   setStateCookie,
@@ -16,6 +17,8 @@ const {
 const { upsertUser } = require("../services/userServices");
 const { sendResponse } = require("../utils");
 const crypto = require("crypto");
+const dayjs = require("dayjs");
+const { database } = require("../db/databse");
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -56,7 +59,77 @@ const getUser = async (req, res) => {
     res.status(500).json(sendResponse.fail("Oauth_failed"));
   }
 };
+
+const rotateRefreshToken = async (req, res) => {
+  try {
+    let oldToken = req.cookies?.refreshToken;
+    if (!oldToken) {
+      console.log("Missing refresh token");
+      return res.status(401).json(sendResponse.fail("Invalid"));
+    }
+
+    const oldHAsh = crypto.createHash("sha256").update(oldToken).digest("hex");
+
+    const rt = await RefreshTokens.findOne({ where: { tokenHash: oldHAsh } });
+    if (!rt) {
+      console.log("Refresh token not found");
+      return res.status(401).json(sendResponse.fail("Invalid"));
+    }
+
+    const isExpired = dayjs(rt.expiresAt).isBefore(dayjs());
+
+    if (isExpired) {
+      console.log("refresh token expired");
+      return res.status(401).json(sendResponse.fail("Invalid"));
+    }
+
+    if (rt.revokedAt) {
+      console.log("The refresh token is revoked");
+      await RefreshTokens.update(
+        { revokedAt: new Date() },
+        {
+          where: {
+            userId: rt.userId,
+            revokedAt: null,
+          },
+        }
+      );
+      return res.status(401).json(sendResponse.fail("Invalid"));
+    }
+
+    const newAscessToken = signToken({ id: rt.userId });
+    const newrefreshToken = refreshToken;
+    const newRefreshHash = crypto
+      .createHash("sha256")
+      .update(newrefreshToken)
+      .digest("hex");
+    const expiresAt = rt.expiresAt;
+
+    await database.transaction(async (t) => {
+      await rt.update(
+        {
+          revokedAt: new Date(),
+          replaceHash: newRefreshHash,
+        },
+        { transaction: t }
+      );
+      await RefreshTokens.create({
+        tokenHash: newRefreshHash,
+        expiresAt: expiresAt,
+        userId: rt.userId,
+      });
+    });
+
+    setJWTcookie(res, newAscessToken, isProd);
+    setRefreshTokenCookie(res, newrefreshToken, isProd);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(sendResponse.fail("refresh_failed"));
+  }
+};
+
 module.exports = {
   consentRedirect,
   getUser,
+  rotateRefreshToken,
 };
--- server/src/routes/authentication.js
+++ server/src/routes/authentication.js
@@ -1,10 +1,14 @@
-const { consentRedirect, getUser } = require("../controllers/authentication");
+const {
+  consentRedirect,
+  getUser,
+  rotateRefreshToken,
+} = require("../controllers/authentication");
 
 const router = require("express").Router();
 
 router.get("/google", consentRedirect);
 router.get("/google/callback", getUser);
-//router.post("/refresh",);
+router.post("/refresh", rotateRefreshToken);
 //router.post("/logout",);
 module.exports = {
   router,
